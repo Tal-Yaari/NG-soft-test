ab1d99777a25d477a3a36122fbdd4bdeff93fd36
Tal Yaari <tal@dorzki.co.il>
2019-03-06 14:15:46 +0200

    Finished test.

--- __src/js/scripts.js
+++ __src/js/scripts.js
@@ -1,5 +1,12 @@
 ( function ( $ ) {
 
+	//After Submit
+	$( '.submit-btn' ).on('click', function() {
+		$( '.update-form' ).addClass('sent');
+		$( '.email-sent' ).addClass('message');
+	});
+
+	//Slick Slider
 	$(document).ready(function() {
 		$( '.banner-carousel' ).slick( {
 			infinite : true,
@@ -10,6 +17,10 @@
 			slidesToScroll : 1,
 			autoplaySpeed : 4000,
 			rtl : true,
+			speed: 1000,
+			cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+			touchThreshold: 100
+
 		} );
 	});
 
@@ -19,6 +30,8 @@
 		$('.banner-carousel .slick-slide').find('video').get(0).pause();
 		var video = $('.banner-carousel .slick-active').find('video').get(0).play();
 	});
+
+
 
 } )( jQuery );
 
