100025846a49652a4b04969b57a503843b53f291
Tal Yaari <tal@dorzki.co.il>
2019-03-06 12:46:19 +0200

    Added iframe to slick slider.

--- __src/js/scripts.js
+++ __src/js/scripts.js
@@ -1,16 +1,24 @@
 ( function ( $ ) {
 
-	window.onload=function() {
+	$(document).ready(function() {
 		$( '.banner-carousel' ).slick( {
 			infinite : true,
 			arrows : false,
 			autoplay : true,
+			adaptiveHeight: true,
 			slidesToShow : 1,
 			slidesToScroll : 1,
-			autoplaySpeed : 1000,
+			autoplaySpeed : 4000,
 			rtl : true,
 		} );
-	};
+	});
+
+	var video = $('.banner-carousel .slick-active').find('iframe').get(0).play();
+
+	$('banner-carousel').on('afterChange', function(event, slick, currentSlide, nextSlide){
+		$('.banner-carousel .slick-slide').find('video').get(0).pause();
+		var video = $('.banner-carousel .slick-active').find('video').get(0).play();
+	});
 
 } )( jQuery );
 
